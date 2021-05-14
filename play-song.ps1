80f880ab3b2c87bf67ebdaefa9d918e64178b727
Tage Tjerngren <Tage.Tjerngren@guldstadsgymnasiet.se>
2021-05-14 11:19:11 +0200

    Clarified powershell script

--- play-song.ps1
+++ play-song.ps1
@@ -1,28 +1,32 @@
-#THEN CHECK IF IT HAS BEEN RUN BY CHECKING IF A FILE WITH TODAYS DATE EXISTS
-#USE THE SIMPLE CONCEPTS OF FM AND EM
-
 echo "Parameters: [$args]"
-
-$currentTime = 830
 
 $validMorning = ((Get-Date '08:30') -lt (Get-Date) -and (Get-Date) -lt (Get-Date '11:00'))
 $validAfternoon = ((Get-Date '12:20') -lt (Get-Date) -and (Get-Date) -lt (Get-Date '15:00'))
 
 if ($validMorning -or $validAfternoon)
 {
-	echo "Correct time"
-	
-	if ( 1 -eq 2 ) #HAS BEEN RUN TODAY
+	if ($validMorning)
 	{
-		Add-Type -AssemblyName presentationCore
-		$mediaPlayer = New-Object system.windows.media.mediaplayer
-		$mediaPlayer.open('C:\Users\TaTje\Downloads\' + $args[0] + '.mp3')
-		$mediaPlayer.Play()
+		echo "It's morning"
+		#Check if it's been run this morning, store the current date in a file called FM or something
 	}
+	if ($validAfternoon)
+	{
+		echo "It's afternoon"
+		#Check if it's been run this morning, store the current date in a file called EM or something
+	}	
+
+	Add-Type -AssemblyName presentationCore
+	$mediaPlayer = New-Object system.windows.media.mediaplayer
+	$mediaPlayer.open('C:\Users\TaTje\Downloads\' + $args[0] + '.mp3')
+	$mediaPlayer.Play()
+}
+else
+{
+	echo "$(Get-Date -Format HH:mm) is not a valid time"
 }
 
-$LastRunTime = Get-Content C:\Users\TaTje\Downloads\current.txt
+#$LastRunTime = Get-Content C:\Users\TaTje\Downloads\current.txt
 
-echo $LastRunTime
+#Get-Date -Format "MMdd HHmm" | Out-File -FilePath C:\Users\TaTje\Downloads\current.txt
 
-Get-Date -Format "MMdd HHmm" | Out-File -FilePath C:\Users\TaTje\Downloads\current.txt
