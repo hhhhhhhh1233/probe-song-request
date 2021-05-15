ebefa12b115ac6b11495a67161903dfb7b186653
Tage Tjerngren <tage.tjerngren@guldstadsgymnasiet.se>
2021-05-15 22:09:59 +0200

    Made it work on any system with mpg123 installed, regardless of OS

--- play-song.ps1
+++ play-song.ps1
@@ -8,23 +8,13 @@ function Play-Tune
         	$Tune
 	)
 
-	if ($IfWindows)
-	{
-		Add-Type -AssemblyName presentationCore
-		$mediaPlayer = New-Object system.windows.media.mediaplayer
-		$mediaPlayer.open('songs/' + $Tune + '.mp3')
-		$mediaPlayer.Play()
-	}
-	elseif ($IsLinux)
-	{
-		mpg123 $('songs/' + $Tune + '.mp3')
-	}
+	mpg123 $('songs/' + $Tune + '.mp3')
 
 	Write-Output "Parameters: [$Tune]"
 }
 
 $validMorning = ((Get-Date '08:30') -lt (Get-Date) -and (Get-Date) -lt (Get-Date '11:00'))
-$validAfternoon = ((Get-Date '12:20') -lt (Get-Date) -and (Get-Date) -lt (Get-Date '21:00'))
+$validAfternoon = ((Get-Date '12:20') -lt (Get-Date) -and (Get-Date) -lt (Get-Date '15:00'))
 
 $PlayFlag = "flags/$(get-date -Format MMdd)-$($args[0])"
 
