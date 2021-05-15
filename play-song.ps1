b6836e3e0fcc1fc8d5db7d57559afeaeb95d578c
Tage Tjerngren <Tage.Tjerngren@guldstadsgymnasiet.se>
2021-05-15 16:31:18 +0200

    Fixed so song will only play per class

--- play-song.ps1
+++ play-song.ps1
@@ -1,32 +1,54 @@
-echo "Parameters: [$args]"
+Write-Output "Parameters: [$args]"
+
+function Play-Tune {
+
+	param (
+        $Tune
+    )
+
+	Add-Type -AssemblyName presentationCore
+	$mediaPlayer = New-Object system.windows.media.mediaplayer
+	$mediaPlayer.open('C:\Users\TaTje\Downloads\' + $Tune + '.mp3')
+	$mediaPlayer.Play()
+	Write-Output "Parameters: [$Tune]"
+}
 
 $validMorning = ((Get-Date '08:30') -lt (Get-Date) -and (Get-Date) -lt (Get-Date '11:00'))
-$validAfternoon = ((Get-Date '12:20') -lt (Get-Date) -and (Get-Date) -lt (Get-Date '15:00'))
+$validAfternoon = ((Get-Date '12:20') -lt (Get-Date) -and (Get-Date) -lt (Get-Date '17:00'))
+
+$PlayFlag = "$(get-date -Format MMdd)-$($args[0])"
+Write-Output FM-$PlayFlag
 
 if ($validMorning -or $validAfternoon)
 {
 	if ($validMorning)
 	{
-		echo "It's morning"
-		#Check if it's been run this morning, store the current date in a file called FM or something
+		if (test-path FM-$PlayFlag)
+		{
+			Write-Output "Already run today!"
+		}
+		else
+		{
+			Play-Tune $args[0]
+			Write-Output $null >> FM-$PlayFlag
+		}
 	}
+
 	if ($validAfternoon)
 	{
-		echo "It's afternoon"
-		#Check if it's been run this morning, store the current date in a file called EM or something
+		if (test-path EM-$PlayFlag)
+		{
+			Write-Output "Already run today!"
+		}
+		else
+		{
+			Play-Tune $args[0]
+			Write-Output $null >> EM-$PlayFlag
+		}
 	}	
-
-	Add-Type -AssemblyName presentationCore
-	$mediaPlayer = New-Object system.windows.media.mediaplayer
-	$mediaPlayer.open('C:\Users\TaTje\Downloads\' + $args[0] + '.mp3')
-	$mediaPlayer.Play()
 }
 else
 {
-	echo "$(Get-Date -Format HH:mm) is not a valid time"
+	Write-Output "$(Get-Date -Format HH:mm) is not a valid time"
 }
-
-#$LastRunTime = Get-Content C:\Users\TaTje\Downloads\current.txt
-
-#Get-Date -Format "MMdd HHmm" | Out-File -FilePath C:\Users\TaTje\Downloads\current.txt
 
