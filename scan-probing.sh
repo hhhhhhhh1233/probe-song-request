c4a3126285bba2df0f422d77d517a396d6736fff
hhhhhhhh1233 <58606478+hhhhhhhh1233@users.noreply.github.com>
2021-05-17 17:54:56 +0200

    Update scan-probing.sh

--- scan-probing.sh
+++ scan-probing.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-airmon-ng start wlan0
 
 MACS=(':aa:aa ' ':bb:bb ' ':cc:cc ' ':dd:dd ' ':ee:ee ' ':ff:ff ')
 SONG=('person1' 'person2' 'person3' 'person4' 'person5' 'person6')
 
+MONITOR_CARD=<THE WIRELESS INTERFACE FOR MONITORING>
+
 NODE_SERVER=<IP ADDRESS OF WEB SERVER>
+
+
+airmon-ng start $MONITOR_CARD
 
 while [ : ]; do
 	TSHARK_RESULTS=$(tshark -c 20 -i wlan0mon | grep "Probe Request")
