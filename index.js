571cb9b36961e7c4b5017181a94fa344ebee1b4f
Gaizee <55480190+Gaizee@users.noreply.github.com>
2021-05-18 09:30:15 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,71 +3,17 @@ const app = express()
 const port = 3000
 var spawn = require("child_process").spawn,child;
 
-app.get('/tage',   (req, res) => {
-  res.send('Hello World!\n');
-  console.log("worky_tage");
+//Copy paste this app.get to add more people.
+app.get('/name', (req, res) => {
+  res.send(`Works for name`);
+  console.log(`Works for name`);
 
-child = spawn("powershell.exe",[" C:\\Users\\administrator\\Desktop\\probe-song-request-main\\play-song.ps1 tage"]);
-  child.stdout.on("data",function(data){
+child = spawn("powershell.exe",["Path_to_Play-song.ps1 name"]); //Replace with path and name of person in mac list.
+child.stdout.on("data",function(data){
     console.log("Powershell Data: " + data);
 });
 child.stdin.end(); //end input
 
-})
-
-app.get('/tim',   (req, res) => {
-  res.send('Hello World!\n');
-  console.log("worky_tim");
-
-child = spawn("powershell.exe",[" C:\\Users\\administrator\\Desktop\\probe-song-request-main\\play-song.ps1 tim"]);
-  child.stdout.on("data",function(data){
-    console.log("Powershell Data: " + data);
-});
-child.stdin.end(); //end input
-})
-
-app.get('/mikke',   (req, res) => {
-  res.send('Hello World!\n');
-  console.log("worky_mikke");
-
-child = spawn("powershell.exe",["C:\\Users\\administrator\\Desktop\\probe-song-request-main\\play-song.ps1 mikke"]);
-  child.stdout.on("data",function(data){
-    console.log("Powershell Data: " + data);
-});
-child.stdin.end(); //end input
-})
-
-app.get('/linus',   (req, res) => {
-  res.send('Hello World!\n');
-  console.log("worky_mikke");
-
-child = spawn("powershell.exe",[" C:\\Users\\administrator\\Desktop\\probe-song-request-main\\play-song.ps1 linus"]);
-  child.stdout.on("data",function(data){
-    console.log("Powershell Data: " + data);
-});
-child.stdin.end(); //end input
-})
-
-app.get('/lukas',   (req, res) => {
-  res.send('Hello World!\n');
-  console.log("worky_mikke");
-
-child = spawn("powershell.exe",[" C:\\Users\\administrator\\Desktop\\probe-song-request-main\\play-song.ps1 lukas"]);
-  child.stdout.on("data",function(data){
-    console.log("Powershell Data: " + data);
-});
-child.stdin.end(); //end input
-})
-
-app.get('/elias',   (req, res) => {
-  res.send('Hello World!\n');
-  console.log("worky_mikke");
-
-child = spawn("powershell.exe",[" C:\\Users\\administrator\\Desktop\\probe-song-request-main\\play-song.ps1 elias"]);
-  child.stdout.on("data",function(data){
-    console.log("Powershell Data: " + data);
-});
-child.stdin.end(); //end input
 })
 
 app.listen(port, () => {
